8fa0f040eb9ec6d273fd9af1c744d29971d50391
Sushmita Yadav <yadavsushmita293@gmail.com>
2024-04-07 21:48:25 +0530

    adding the projects

---
+++ slot_machine.py
@@ -0,0 +1,72 @@
+import random
+
+print("Hare Krishna")
+
+MAX_LINE = 3
+MAX_BET = 100
+MIN_BET = 1
+
+
+def deposit():
+   while True:
+      amount = input("What would you like to deposit? $")
+      if amount.isdigit():
+         amount = int(amount)
+         if amount > 0:
+            break
+         else:
+            print("Amount must be greater than 0.")
+      else:
+         print("Please enter a number.")
+         
+   return amount
+
+deposit()
+
+def get_number_of_lines():
+   while True:
+      lines = input(f"Enter the number of lines to bet on (1 - {str(MAX_LINE)})?  ")
+      
+      if lines.isdigit():
+         lines = int(lines)
+         if 1 <= lines <= MAX_LINE:
+            break
+         else:
+            print("Enter a valid number of lines. ")
+      else:
+         print("Please enter a number")
+         
+   return lines
+
+def get_bet():
+   while True:
+      amount = input("What would you like to bet on each line? $")
+      if amount.isdigit():
+         amount = int(amount)
+         if MIN_BET <= amount <= MAX_BET:
+            break
+         else:
+            print(f"Amount must be between ${MIN_BET} - ${MAX_LINE}.")
+      else:
+         print("Please enter a number. ")
+   
+   return amount
+   
+def main():
+   balance = deposit()
+   lines = get_number_of_lines()
+   while True:
+      
+      bet = get_bet()
+      total_bet = bet * lines
+   
+      if total_bet > balance:
+         print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+      else:
+         break
+      
+   print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+
+   
+main()
